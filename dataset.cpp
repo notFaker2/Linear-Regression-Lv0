0d1a202fe56867097a455262152bd17981a404de
MD_Abdulla072 <148624131+notFaker2@users.noreply.github.com>
2025-07-15 20:56:50 +0600

    Updated dataset

---
+++ dataset.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+#include<fstream>
+#include<random>
+using namespace std;
+
+int main(){
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<>distrib(1,1000);
+    ofstream file("data.csv");
+    file<<"X,Y\n";
+    for(int i=1;i<=1000;i++){
+        int x=distrib(gen),y;
+        y=2*x+2;
+        file<<x<<","<<y<<"\n";
+    }
+    file.close();
+}
